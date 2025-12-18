95cc200839718b254038182ae2f63a64df044c16
Fazli Zekiqi <fazli.zekiqi@seb.se>
2025-12-18 22:26:00 +0100

    Added steak carousel and description

--- js/main.js
+++ js/main.js
@@ -114,6 +114,23 @@
             }
         }
     });
+
+    $(document).ready(function(){
+        $('.image-carousel').owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 2500,
+            responsive: {
+                0: { items: 1 },
+                600: { items: 2 },
+                1000: { items: 3 }
+            }
+        });
+    });
+
     
 })(jQuery);
 
